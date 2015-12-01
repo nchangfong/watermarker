229a86dfe8177a3db02b88459930045a951f2532
Nick Chang-Fong <nchangfong@gmail.com>
2015-12-01 12:23:55 -0500

    renamed the file, updated readme, cleaned up comments, and deleted redundant code.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # watermarker
-A program I wrote for adding the Hack Western watermark to our photos. 
+
+### Description
+A program I wrote for adding watermarks to photos using the [Python Imaging Library](http://www.pythonware.com/products/pil/) (PIL) 
+
+### Instructions
+Create a target directory for your watermarked photos before executing the program. When ready, run the program by entering `python watermarker.py` in console.
+
+Provide full paths without trailing slashes to your watermark, input, and output directories. The script currently only looks for files ending in `.JPG` but you can edit this in the script itself.
--- watermarker.py
+++ watermarker.py
@@ -3,7 +3,6 @@ A program to place a watermark on photos with the additional option of
 resizing both the watermark and the images.
 """
 
-
 from PIL import Image
 from PIL import ImageDraw
 import glob, os
@@ -22,9 +21,7 @@ outputdir = raw_input("Enter output directory: ")
 
 wm_scaling_factor = float(raw_input("Enter watermark scaling factor: "))
 
-im_scaling_factor = float(raw_input("Enter image scaling factor: "))
-
-
+# new values for watermark after scaling
 new_x = int(wm.size[0]*wm_scaling_factor)
 new_y = int(wm.size[1]*wm_scaling_factor)
 
@@ -33,27 +30,25 @@ wm = wm.resize((new_x,new_y),Image.ANTIALIAS)
 for infile in glob.glob(imagedir + "/*.JPG"):
         file, ext = os.path.splitext(os.path.basename(infile))
         im = Image.open(infile)
-
-       # print "if statement: " + str(max(im.size[0],im.size[1])/float(min(im.size[0],im.size[1])))
-        
+       
+        # calculates the scaling factor by finding dividing the longer axis of input
+        # photo over pixel length of desired output
         im_scaling_factor = max(im.size[0],im.size[1])/float(1920)
 
-      #  print "im scaling factor: " + str(im_scaling_factor)
-
-        #applying scaling factor to image
-        #if im.size[0] > 1920 or im.size[1] > 1280:
+        # applying scaling factor to image
     	im_x = int(im.size[0]/im_scaling_factor)
     	im_y = int(im.size[1]/im_scaling_factor)
 
+        # resize the image
         im = im.resize((im_x,im_y), Image.ANTIALIAS)
 
-
+        # correcting aspect ratios for odd cases
         if (im.size[0]>im.size[1]):
                 im = im.crop([0,0,1920,1280])
         elif (im.size[1]>im.size[0]):
                 im = im.crop([0,0,1280,1920])
 
-        #position for watermark
+        # position for watermark
         wm_x = im.size[0]-wm.size[0]-7
         wm_y = im.size[1]-wm.size[1]-5
         wm_pos = (wm_x,wm_y)
