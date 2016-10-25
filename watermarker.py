8f3f592138028b2e5400779711162f52f5a238c0
Nick Chang-Fong <nchangfong@gmail.com>
2016-10-25 17:27:05 -0400

    added prompts to change parameters
    shouldn't require editing source as much for tweaking.

--- watermarker.py
+++ watermarker.py
@@ -1,30 +1,25 @@
 """
-A program to place a watermark on photos with the additional option of 
-resizing both the watermark and the images.
+places a watermark on photos with the option to resize both the watermark and the images.
 """
 
 from PIL import Image
 from PIL import ImageDraw
 import glob, os
 
-watermark = "./watermarks/" + raw_input("Enter filename of watermark: ")
+watermark = "./watermarks/" + raw_input("Enter filename of watermark including extension: ")
 wm = Image.open(watermark)
 
 # directory where images are to be watermarked
-# imagedir = raw_input("Enter image directory: ")
-imagedir = "./input"
+imagedir = raw_input("Enter path to input directory: ")
 
 
 # where watermarked images will end up
-# user has to make sure directory already exists
-# outputdir = raw_input("Enter output directory: ")
 outputdir = "./output"
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
 
-
-# wm_scaling_factor = float(raw_input("Enter watermark scaling factor: "))
-
-#0.8 is hardcoded value used for hack western
-wm_scaling_factor = 0.8
+# factor by which to scale watermark
+wm_scaling_factor = float(raw_input("Enter watermark scaling factor (HW default 0.8): "))
 
 # new values for watermark after scaling
 new_x = int(wm.size[0]*wm_scaling_factor)
@@ -32,13 +27,17 @@ new_y = int(wm.size[1]*wm_scaling_factor)
 
 wm = wm.resize((new_x,new_y),Image.ANTIALIAS)
 
-for infile in glob.glob(imagedir + "/*.JPG"):
+# distance between right and bottom edge of watermark and original photo
+wm_x_offset = float(raw_input("Enter watermark x offset (HW default: 7): "))
+wm_y_offset = float(raw_input("Enter watermark y offset (HW default: 5): "))
+
+for infile in glob.glob(imagedir + "/*.[jJ][pP][gG]"):
         file, ext = os.path.splitext(os.path.basename(infile))
         im = Image.open(infile)
-       
+
         # calculates the scaling factor by finding dividing the longer axis of input
         # photo over pixel length of desired output
-	# scaling to 1920 is for hack western facebook photo uploads
+        # scaling to 1920 is for hack western facebook photo uploads
         im_scaling_factor = max(im.size[0],im.size[1])/float(1920)
 
         # applying scaling factor to image
@@ -48,15 +47,15 @@ for infile in glob.glob(imagedir + "/*.JPG"):
         # resize the image
         im = im.resize((im_x,im_y), Image.ANTIALIAS)
 
-        # correcting aspect ratios for odd cases
-        if (im.size[0]>im.size[1]):
-                im = im.crop([0,0,1920,1280])
-        elif (im.size[1]>im.size[0]):
-                im = im.crop([0,0,1280,1920])
+        # cropping for a few photos with unexpected aspect ratios
+        # if (im.size[0]>im.size[1]):
+        #         im = im.crop([0,0,1920,1280])
+        # elif (im.size[1]>im.size[0]):
+        #         im = im.crop([0,0,1280,1920])
 
-        # position for watermark
-        wm_x = im.size[0]-wm.size[0]-7
-        wm_y = im.size[1]-wm.size[1]-5
+        # top-left position of watermark
+        wm_x = im.size[0]-wm.size[0]-wm_x_offset
+        wm_y = im.size[1]-wm.size[1]-wm_y_offset
         wm_pos = (wm_x,wm_y)
         im.paste(wm, wm_pos, mask=wm)
         im.save(outputdir + "/" + file + "-wm.jpg")
